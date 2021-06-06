17c94799e751417b585da58bd298e91bb74f9c11
Rodrigo Surita <rodrigosurita@live.com>
2021-06-06 16:09:10 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+import { ClientOptions, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
+
+const microserviceOptions: ClientOptions = {
+  transport: Transport.TCP,
+  options:{
+    host:'127.0.0.1',
+    port:8877
+  }
+};
+
+const client = ClientProxyFactory.create(microserviceOptions);
+
+client
+  // .send<ReturnType, ParamType>(pattern, param)
+  .send<number, number[]>('add', [1,2,3])
+  .subscribe(result => logger.log(result));
